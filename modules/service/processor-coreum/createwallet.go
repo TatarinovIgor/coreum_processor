6a5ed7fc61fc6da11bd191460a9b4397d4a0f9b8
Vladimir Tatarinov <vladimir.tatarinov@rock-west.com>
2023-10-23 20:08:14 +0300

    fixed wallet generation for multisign

--- modules/service/processor-coreum/createwallet.go
+++ modules/service/processor-coreum/createwallet.go
@@ -18,7 +18,7 @@ func (s CoreumProcessing) CreateWallet(ctx context.Context, merchantID, external
 
 	wallet := service.Wallet{Blockchain: s.blockchain}
 
-	WalletSeed, WalletAddress, threshold, err := s.createCoreumWallet(ctx, externalId, multiSignAddresses)
+	WalletSeed, WalletAddress, address, threshold, err := s.createCoreumWallet(ctx, externalId, multiSignAddresses)
 	if err != nil {
 		return nil, err
 	}
@@ -32,7 +32,7 @@ func (s CoreumProcessing) CreateWallet(ctx context.Context, merchantID, external
 		return nil, err
 	}
 
-	_, err = s.store.Put(merchantID, externalId, wallet.WalletAddress, key)
+	_, err = s.store.Put(merchantID, externalId, address, key)
 	if err != nil {
 		return nil, err
 	}
@@ -41,50 +41,16 @@ func (s CoreumProcessing) CreateWallet(ctx context.Context, merchantID, external
 }
 
 func (s CoreumProcessing) createCoreumWallet(ctx context.Context, externalId string,
-	multiSignAddresses service.FuncMultiSignAddrCallback) (string, string, float64, error) {
+	multiSignAddresses service.FuncMultiSignAddrCallback) (string, string, string, float64, error) {
 	threshold := 0.
-	if multiSignAddresses != nil {
-		addresses, threshold, err := multiSignAddresses(s.blockchain, externalId)
-		if err != nil {
-			return "", "", threshold, fmt.Errorf("can't create Coreum multising Wallet, error: %v", err)
-		} else if addresses != nil && len(addresses) > 0 {
-			signKeys := []types.PubKey{}
-			// create multi sig wallet
-			for key := range addresses {
-				accAddress, err := sdk.AccAddressFromBech32(key)
-				if err != nil {
-					return "", "", threshold, fmt.Errorf(
-						"can't create Coreum multising Wallet from provided key: %s, error: %w", key, err)
-				}
-				info, err := client.GetAccountInfo(ctx, s.clientCtx, accAddress)
-				if err != nil {
-					return "", "", threshold, fmt.Errorf(
-						"can't create Coreum multising Wallet from info key: %s, error: %w", key, err)
-				}
-				signKeys = append(signKeys, info.GetPubKey())
-			}
-			pubKey := amomultisig.NewLegacyAminoPubKey(2, signKeys)
-			Info, err := s.clientCtx.Keyring().SaveMultisig("multi-sign", pubKey)
-			if err != nil {
-				return "", "", threshold, fmt.Errorf("can't save Coreum multising Wallet, error: %v", err)
-			}
-			defer func() { _ = s.clientCtx.Keyring().DeleteByAddress(Info.GetAddress()) }()
-			// Validate address
-			_, err = sdk.AccAddressFromBech32(Info.GetAddress().String())
-			if err != nil {
-				return "", "", threshold, fmt.Errorf("can't validate Coreum multising Wallet, error: %v", err)
-			}
-			return "", Info.GetAddress().String(), threshold, nil
-		}
-	}
 
 	entropy, err := bip39.NewEntropy(256)
 	if err != nil {
-		return "", "", threshold, err
+		return "", "", "", threshold, err
 	}
 	mnemonic, err := bip39.NewMnemonic(entropy)
 	if err != nil {
-		return "", "", threshold, err
+		return "", "", "", threshold, err
 	}
 
 	Info, err := s.clientCtx.Keyring().NewAccount(
@@ -96,15 +62,50 @@ func (s CoreumProcessing) createCoreumWallet(ctx context.Context, externalId str
 	)
 
 	if err != nil {
-		return "", "", threshold, err
+		return "", "", "", threshold, err
 	}
 	defer func() { _ = s.clientCtx.Keyring().DeleteByAddress(Info.GetAddress()) }()
+	walletAddress := Info.GetAddress().String()
+	if multiSignAddresses != nil {
+		addresses, threshold, err := multiSignAddresses(s.blockchain, externalId)
+		if err != nil {
+			return "", "", "", threshold, fmt.Errorf("can't create Coreum multising Wallet, error: %v", err)
+		} else if addresses != nil && len(addresses) > 0 && threshold > 0 {
+			signKeys := []types.PubKey{Info.GetPubKey()}
+			// create multi sig wallet
+			for key := range addresses {
+				accAddress, err := sdk.AccAddressFromBech32(key)
+				if err != nil {
+					return "", "", "", threshold, fmt.Errorf(
+						"can't create Coreum multising Wallet from provided key: %s, error: %w", key, err)
+				}
+				info, err := client.GetAccountInfo(ctx, s.clientCtx, accAddress)
+				if err != nil {
+					return "", "", "", threshold, fmt.Errorf(
+						"can't create Coreum multising Wallet from info key: %s, error: %w", key, err)
+				}
+				signKeys = append(signKeys, info.GetPubKey())
+			}
+			pubKey := amomultisig.NewLegacyAminoPubKey(2, signKeys)
+			Info, err := s.clientCtx.Keyring().SaveMultisig("multi-sign", pubKey)
+			if err != nil {
+				return "", "", "", threshold, fmt.Errorf("can't save Coreum multising Wallet, error: %v", err)
+			}
+			defer func() { _ = s.clientCtx.Keyring().DeleteByAddress(Info.GetAddress()) }()
+			// Validate address
+			_, err = sdk.AccAddressFromBech32(Info.GetAddress().String())
+			if err != nil {
+				return "", "", "", threshold, fmt.Errorf("can't validate Coreum multising Wallet, error: %v", err)
+			}
+			return "", walletAddress, Info.GetAddress().String(), threshold, nil
+		}
+	}
 
 	// Validate address
 	_, err = sdk.AccAddressFromBech32(Info.GetAddress().String())
 	if err != nil {
-		return "", "", threshold, err
+		return "", "", "", threshold, err
 	}
 
-	return mnemonic, Info.GetAddress().String(), threshold, nil
+	return mnemonic, walletAddress, Info.GetAddress().String(), threshold, nil
 }
